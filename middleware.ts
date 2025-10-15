28f28cf38b1570644b1e120757096d84a3ff12b8
SH701 <rkrk7636@naver.com>
2025-10-15 22:06:49 +0900

    로그인 없이 기능 일부 가능

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,22 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
+const protectedRoutes = createRouteMatcher(["/me(.*)", "/post(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
+  if (!userId && protectedRoutes(req)) {
+    const signInUrl = new URL("/error", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
   if (userId && req.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/main", req.url));
   }
+
   return NextResponse.next();
 });
-
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
